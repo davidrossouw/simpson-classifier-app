5fde15f5f398eb1a62162f962bb9a9c10c5bb4c7
davidrossouw <davidrossouw@gmail.com>
2019-08-23 18:18:15 -0400

    Initial Commit

---
+++ SwiftImageAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftImageAPI
+//
+//  Created by David Rossouw on 2019-08-23.
+//  Copyright © 2019 David Rossouw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
